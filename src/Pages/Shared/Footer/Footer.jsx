20e616196855cbcc83bcc8e777f0afecba002aaf
originalmahim <tareqazizmahim@gmail.com>
2023-11-23 23:45:10 +0600

    Created Footer Section Successfully

--- src/Pages/Shared/Footer/Footer.jsx
+++ src/Pages/Shared/Footer/Footer.jsx
@@ -1,7 +1,34 @@
+
+
 const Footer = () => {
           return (
-          <div>
-           <h1>This Is Footer Section</h1>                   
+          <div className="max-w-7xl mx-auto text-xl">
+          <footer className="footer p-10  text-base-content">
+          <aside>
+          <img className="w-44" src="https://starbelly.windstripedesign.ro/images/ui/logo.svg"  />
+          <p>ACME Industries Ltd.<br/>Providing reliable tech since 1992</p>
+          </aside> 
+          <nav>
+          <header className="footer-title">Services</header> 
+          <a className="link link-hover">Branding</a>
+          <a className="link link-hover">Design</a>
+          <a className="link link-hover">Marketing</a>
+          <a className="link link-hover">Advertisement</a>
+          </nav> 
+          <nav>
+          <header className="footer-title">Company</header> 
+          <a className="link link-hover">About us</a>
+          <a className="link link-hover">Contact</a>
+          <a className="link link-hover">Jobs</a>
+          <a className="link link-hover">Press kit</a>
+          </nav> 
+          <nav>
+          <header className="footer-title">Legal</header> 
+          <a className="link link-hover">Terms of use</a>
+          <a className="link link-hover">Privacy policy</a>
+          <a className="link link-hover">Cookie policy</a>
+          </nav>
+          </footer>                             
           </div>
           );
 };
