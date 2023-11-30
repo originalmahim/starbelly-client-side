bcd6649f936742e69e978aef6f3a259670d5ec13
originalmahim <tareqazizmahim@gmail.com>
2023-11-30 15:31:32 +0600

    Updated Dashborad layout for all devices . responsived it successfully

--- src/Pages/Dashboard/Dashboard.jsx
+++ src/Pages/Dashboard/Dashboard.jsx
@@ -1,69 +1,236 @@
-
 import { useContext, useEffect, useState } from "react";
 import { NavLink, Outlet } from "react-router-dom";
 import { AuthContex } from "../Providers/AuthProvider";
 
 const Dashboard = () => {
-
-  const {user} = useContext(AuthContex);
-
+  const { user } = useContext(AuthContex);
   const [info, setInfo] = useState(null);
 
   useEffect(() => {
     fetch(`http://localhost:5000/users/${user?.email}`)
-    .then((res) => res.json())
-    .then((data) => {
-      setInfo(data);
-    })
-    .catch((error) => {
-    console.error("Error fetching data:", error);
-    });
-    }, [user?.email]);
-          
+      .then((res) => res.json())
+      .then((data) => {
+        setInfo(data);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  }, [user?.email]);
 
-          return (
-          
-            <div className="min-h-[70vh]">
-              {/* Navigation */}
-              <nav className="bg-yellow-400 p-4">
-                <div className="container mx-auto">
-                  <div className="text-black text-center">
-                    <h1 className="text-2xl font-semibold">Wellcome To Dashboard</h1>
-                  </div>
-                </div>
-              </nav>
-        
-              {/* Content */}
-              <div className="max-w-7xl mx-auto mt-8">
-                {/* Your routing logic goes here */}
-                {/* For simplicity, I'll create a basic tab structure */}
-                <div className="flex">
-                  <div className="w-1/4 text-xl font-semibold">
-                    {/* Sidebar */}
-                    { info?.role == 'admin' ? <ul>
-                      <li><NavLink to= '/dashboard/profile'  className="block p-4">Admin Profile</NavLink></li>
-                      <li><NavLink to='/dashboard/manageuser'  className="block p-4">Manage Users</NavLink></li>
-                      <li><NavLink to='/dashboard/addmeal'  className="block p-4">Add Meal</NavLink></li>
-                      <li><NavLink to= '/dashboard/allmeals'  className="block p-4">All Meals</NavLink></li>
-                      <li><NavLink to='/dashboard/allreviews'  className="block p-4">All Reviews</NavLink></li>
-                      <li><NavLink to= '/dashboard/servemeals'  className="block p-4">Serve Meals</NavLink></li>
-                      <li><NavLink to='/dashboard/upcomingmeals'  className="block p-4">Upcoming Meals</NavLink></li>
-                    </ul>
-                    : 
-                    <ul>
-                    <li><NavLink to= '/dashboard/profile'  className="block p-4">My Profile</NavLink></li>
-                    <li><NavLink to= '/dashboard/requested'  className="block p-4">Requested Meals</NavLink></li>
-                    <li><NavLink to= '/dashboard/myreviews'  className="block p-4">My Reviews</NavLink></li>
-                    </ul>
-                    }
-                  </div>
-                  <div className="w-3/4 p-4">
-                    <Outlet></Outlet>
-                  </div>
-                </div>
-              </div>
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+
+  const toggleMobileMenu = () => {
+    setMobileMenuOpen(!mobileMenuOpen);
+  };
+
+  return (
+    <div className="min-h-screen">
+      {/* Navigation */}
+      <nav className="bg-yellow-400 p-4">
+        <div className="container mx-auto">
+          <div className="text-black text-center">
+            <h1 className="text-2xl font-semibold">Welcome To Dashboard</h1>
+          </div>
+        </div>
+      </nav>
+
+      {/* Mobile Menu Button */}
+      <div className="lg:hidden text-right p-4">
+        <button
+          onClick={toggleMobileMenu}
+          className="text-3xl focus:outline-none"
+        >
+          ☰
+        </button>
+      </div>
+
+      {/* Mobile Dropdown Menu */}
+      {mobileMenuOpen && (
+        <div className="lg:hidden">
+          <div className="bg-gray-800 fixed inset-0 z-50 opacity-75"></div>
+          <div className="fixed inset-0 z-50 flex items-center justify-center">
+            <div className="bg-white w-2/3 p-4 rounded-lg shadow">
+              <ul className="space-y-4">
+                {info?.role === "admin" ? (
+                  <>
+                    <li>
+                      <NavLink
+                        to="/dashboard/profile"
+                        onClick={toggleMobileMenu}
+                        className="block p-4"
+                      >
+                        Admin Profile
+                      </NavLink>
+                    </li>
+                    <li>
+                      <NavLink
+                        to="/dashboard/manageuser"
+                        onClick={toggleMobileMenu}
+                        className="block p-4"
+                      >
+                        Manage Users
+                      </NavLink>
+                    </li>
+                    <li>
+                      <NavLink
+                        to="/dashboard/addmeal"
+                        onClick={toggleMobileMenu}
+                        className="block p-4"
+                      >
+                        Add Meal
+                      </NavLink>
+                    </li>
+                    <li>
+                      <NavLink
+                        to="/dashboard/allmeals"
+                        onClick={toggleMobileMenu}
+                        className="block p-4"
+                      >
+                        All Meals
+                      </NavLink>
+                    </li>
+                    <li>
+                      <NavLink
+                        to="/dashboard/allreviews"
+                        onClick={toggleMobileMenu}
+                        className="block p-4"
+                      >
+                        All Reviews
+                      </NavLink>
+                    </li>
+                    <li>
+                      <NavLink
+                        to="/dashboard/servemeals"
+                        onClick={toggleMobileMenu}
+                        className="block p-4"
+                      >
+                        Serve Meals
+                      </NavLink>
+                    </li>
+                    <li>
+                      <NavLink
+                        to="/dashboard/upcomingmeals"
+                        onClick={toggleMobileMenu}
+                        className="block p-4"
+                      >
+                        Upcoming Meals
+                      </NavLink>
+                    </li>
+                  </>
+                ) : (
+                  <>
+                    <li>
+                      <NavLink
+                        to="/dashboard/profile"
+                        onClick={toggleMobileMenu}
+                        className="block p-4"
+                      >
+                        My Profile
+                      </NavLink>
+                    </li>
+                    <li>
+                      <NavLink
+                        to="/dashboard/requested"
+                        onClick={toggleMobileMenu}
+                        className="block p-4"
+                      >
+                        Requested Meals
+                      </NavLink>
+                    </li>
+                    <li>
+                      <NavLink
+                        to="/dashboard/myreviews"
+                        onClick={toggleMobileMenu}
+                        className="block p-4"
+                      >
+                        My Reviews
+                      </NavLink>
+                    </li>
+                  </>
+                )}
+              </ul>
             </div>
-          );
-        }
-        
-        export default Dashboard;
+          </div>
+        </div>
+      )}
+
+      {/* Content */}
+      <div className="lg:max-w-7xl mx-auto mt-8">
+        {/* Your routing logic goes here */}
+        {/* For simplicity, I'll create a basic tab structure */}
+        <div className="lg:flex">
+          <div className="lg:w-1/4 p-4 hidden lg:block">
+            {/* Sidebar */}
+            {info?.role === "admin" ? (
+              <ul>
+                <li>
+                  <NavLink to="/dashboard/profile" className="block p-4">
+                    Admin Profile
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink to="/dashboard/manageuser" className="block p-4">
+                    Manage Users
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink to="/dashboard/addmeal" className="block p-4">
+                    Add Meal
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink to="/dashboard/allmeals" className="block p-4">
+                    All Meals
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink to="/dashboard/allreviews" className="block p-4">
+                    All Reviews
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink to="/dashboard/servemeals" className="block p-4">
+                    Serve Meals
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink
+                    to="/dashboard/upcomingmeals"
+                    className="block p-4"
+                  >
+                    Upcoming Meals
+                  </NavLink>
+                </li>
+              </ul>
+            ) : (
+              <ul>
+                <li>
+                  <NavLink to="/dashboard/profile" className="block p-4">
+                    My Profile
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink to="/dashboard/requested" className="block p-4">
+                    Requested Meals
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink to="/dashboard/myreviews" className="block p-4">
+                    My Reviews
+                  </NavLink>
+                </li>
+              </ul>
+            )}
+          </div>
+          <div className="lg:w-3/4 p-4">
+            <Outlet></Outlet>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Dashboard;
+
+
